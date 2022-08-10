777d7d3eb2054f77ff6ed6f3f3d2e8490a05b7c0
João Gabriel <main.joaogabrielsg@gmail.com>
2022-08-09 21:46:53 -0300

    feat: configure the theme and global style

---
+++ src/styles/global.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle, css } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`${({ theme }) => css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    background: ${theme.colors["gray-100"]};
+    color: ${theme.colors["gray-700"]};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body,
+  input,
+  textarea,
+  button {
+    font-family: ${theme.fonts.text};
+    font-weight: 400;
+    font-size: ${theme.fontSize[10]};
+  }
+`}`;
