238aca9b2d52ad6ae0e62dbad7ca19dc378bf22e
João Gabriel <main.joaogabrielsg@gmail.com>
2022-08-10 23:03:56 -0300

    feat: add default layout styles

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,3 +1,11 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  max-width: 112rem;
+  padding: 0 2.4rem;
+  margin: 0 auto;
+
+  display: flex;
+  flex-direction: column;
+`;
