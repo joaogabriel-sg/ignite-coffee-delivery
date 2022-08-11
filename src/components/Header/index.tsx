90136b90debefff787d460bba5a130345f3a522a
João Gabriel <main.joaogabrielsg@gmail.com>
2022-08-10 23:05:10 -0300

    feat: create header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,24 @@
+import { MapPin, ShoppingCart } from "phosphor-react";
+
+import logoCoffeeDelivery from "../../assets/images/brand/logo-coffee-delivery.svg";
+
 import * as S from "./styles";
 
 export function Header() {
-  return <S.Container>Header</S.Container>;
+  return (
+    <S.Container>
+      <img src={logoCoffeeDelivery} />
+
+      <S.MenuContainer>
+        <S.LocationButton type="button">
+          <MapPin size={22} weight="fill" />
+          Boa Viagem, CE
+        </S.LocationButton>
+
+        <S.CartButton type="button">
+          <ShoppingCart size={22} weight="fill" />
+        </S.CartButton>
+      </S.MenuContainer>
+    </S.Container>
+  );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,59 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const Container = styled.header``;
+export const Container = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1.2rem;
+
+  padding: 3.2rem 0;
+`;
+
+export const MenuContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.2rem;
+`;
+
+export const LocationButton = styled.button`
+  ${({ theme }) => css`
+    background: ${theme.colors["secondary-light"]};
+    height: 3.8rem;
+    padding: 0.8rem;
+    border: 0;
+    border-radius: 6px;
+
+    display: flex;
+    align-items: center;
+    gap: 4px;
+
+    cursor: pointer;
+
+    font-size: ${theme.fontSize[14]};
+    color: ${theme.colors["secondary-dark"]};
+
+    > svg {
+      flex-shrink: 0;
+      color: ${theme.colors["secondary"]};
+    }
+  `}
+`;
+
+export const CartButton = styled.button`
+  ${({ theme }) => css`
+    background: ${theme.colors["primary-light"]};
+    width: 3.8rem;
+    height: 3.8rem;
+    border: 0;
+    border-radius: 6px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-shrink: 0;
+
+    cursor: pointer;
+
+    color: ${theme.colors["primary-dark"]};
+  `}
+`;
